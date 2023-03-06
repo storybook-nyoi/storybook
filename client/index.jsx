4fc06f27b9f3ebffb745204767859dd601f4893e
Nicholas Cathcart <nicholascathcart@Nicholass-MacBook-Air.local>
2023-03-06 14:04:50 -0500

    updated index for initial setup

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-const index = () => {
-  return <div>index</div>;
-};
-
-export default index;
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
