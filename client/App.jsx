0c0ff7b73e94486530cff87a0f451155efa76260
Dillon <93858218+d-hp@users.noreply.github.com>
2023-03-07 11:14:30 -0500

    Implemented middleware to provide OpenAI with prompt to generate text with davinci AI model

--- client/App.jsx
+++ client/App.jsx
@@ -1,38 +1,52 @@
-import React from 'react';
-import ListItem from './components/ListItem.jsx'
-const App = () => {
+import axios from 'axios';
+import React, { useState } from 'react';
+import ListItem from './components/ListItem.jsx';
 
+const App = () => {
   const storyThemes = ['Princess', 'Ninjas', 'Horses', 'Pirates', 'Explorers'];
 
   const storyThemeComponents = [];
 
-  for (let i=0; i<storyThemes.length; i++){
-    storyThemeComponents.push(<ListItem name={storyThemes[i]}/>)
-  }
-  
-  return(
-    
+  // for (let i = 0; i < storyThemes.length; i++) {
+  //   storyThemeComponents.push(<ListItem name={storyThemes[i]} />);
+  // }
+
+  return (
     <div>
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      <div className='navbar bg-base-100'>
+        <div className='navbar-start'>
+          <div className='dropdown'>
+            <label tabIndex={0} className='btn btn-ghost btn-circle'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 6h16M4 12h16M4 18h7'
+                />
+              </svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              {storyThemeComponents}
+            <ul
+              tabIndex={0}
+              className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'
+            >
+              // {storyThemeComponents}
             </ul>
           </div>
         </div>
-        <div className="navbar-start">
-          <a className="btn btn-ghost normal-case text-xl text-start">Story Book</a>
+        <div className='navbar-start'>
+          <a className='btn btn-ghost normal-case text-xl text-start'>
+            Story Book
+          </a>
         </div>
+      </div>
     </div>
-      
-      
-    </div>
-    
-  ) 
+  );
 };
-
 export default App;
--- server/controllers/createController.js
+++ server/controllers/createController.js
@@ -1,12 +1,41 @@
+const config = require('./util/config');
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: `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`,
+});
+
 const createController = {};
 
-createController.getStory = (req, res, next) => {
-  console.log('In getStory');
-  return next();
+createController.getStory = async (req, res, next) => {
+  const prompt =
+    'Tell me a story about a princess that goes out into the sea and finds an opportunity';
+
+  try {
+    const openai = new OpenAIApi(configuration);
+    if (prompt === null) {
+      console.log('No prompt provided.');
+      return;
+    }
+    const res = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt,
+    });
+    const firstStory = res.data.choices[0].text;
+    res.locals.story = firstStory;
+    return next();
+  } catch (err) {
+    return next({
+      log: `OpenAI API 'getStory' error in createController.getStory middleware: ${err.message}`,
+      status: err.response.status,
+      message: {
+        err: 'An error occurred, pelase check server logs for details.',
+      },
+    });
+  }
 };
 
 createController.getImages = (req, res, next) => {
-  console.log('In getImages');
   return next();
 };
 
